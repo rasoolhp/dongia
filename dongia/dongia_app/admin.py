f6ccb7668c3aa0272e63793d34587620e094d3c0
RasoolHP <rasoolhp4@gmail.com>
2018-09-21 04:08:28 +0430

    Beautification for admin.py

--- dongia/dongia_app/admin.py
+++ dongia/dongia_app/admin.py
@@ -2,12 +2,26 @@ from django.contrib import admin
 from dongia_app.models import *
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ("name",)
+    list_display = (
+        "name",
+    )
 class DongAdmin(admin.ModelAdmin):
-    list_display = ("name","amount","donger","dong_per_person","dongia_list")
+    list_display = (
+        "name",
+        "amount",
+        "donger",
+        "dong_per_person",
+        "dongia_list",
+    )
 class DongRecordAdmin(admin.ModelAdmin):
-    list_display = ("from_user","to_user","amount","for_dong","paid")
+    list_display = (
+        "from_user",
+        "to_user",
+        "amount",
+        "for_dong",
+        "paid",
+    )
 
-admin.site.register(User,UserAdmin)
-admin.site.register(Dong,DongAdmin)
-admin.site.register(DongRecord,DongRecordAdmin)
+admin.site.register(User, UserAdmin)
+admin.site.register(Dong, DongAdmin)
+admin.site.register(DongRecord, DongRecordAdmin)
