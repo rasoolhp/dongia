271e7d7bfec2bf4bbc4767ea6bed5ebeb5f21f71
RasoolHP <rasoolhp4@gmail.com>
2018-09-21 02:31:09 +0430

    we fully added Dongs Model

--- dongia/dongia_app/admin.py
+++ dongia/dongia_app/admin.py
@@ -3,5 +3,8 @@ from dongia_app.models import *
 
 class UserAdmin(admin.ModelAdmin):
     list_display = ("name",)
+class DongAdmin(admin.ModelAdmin):
+    list_display = ("name","amount","donger","dong","dongia_list")
 
 admin.site.register(User,UserAdmin)
+admin.site.register(Dong,DongAdmin)
--- dongia/dongia_app/models.py
+++ dongia/dongia_app/models.py
@@ -4,3 +4,17 @@ class User(models.Model):
     name = models.CharField(max_length=50)
     def __str__(self):
         return self.name
+
+class Dong(models.Model):
+    name = models.CharField(max_length=50)
+    amount = models.FloatField()
+    dongia = models.ManyToManyField(User)
+    donger = models.ForeignKey(User,on_delete=models.CASCADE,related_name="donger")
+    def dongia_list(self):
+        return " - ".join( [p.name for p in self.dongia.all()] )
+    @property
+    def dong(self):
+        tedad = self.dongia.count()
+        return self.amount/tedad if tedad else None
+    def __str__(self):
+            return self.name
